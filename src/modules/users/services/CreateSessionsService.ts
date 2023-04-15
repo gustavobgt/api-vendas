d336fef64d457567c2cf40680c54ac7e0c295dab
gustavobgt <gustavo_bgt97@hotmail.com>
2023-04-15 10:28:56 -0300

    feat: add jwt sign and token return from sessions create service

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,20 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -24,7 +30,12 @@ class CreateSessionsService {
       throw new AppError('Incorrect email adress or password', 401);
     }
 
-    return user;
+    const token = sign({}, '7555dc11643c7aadaaac010cffc829bc', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
