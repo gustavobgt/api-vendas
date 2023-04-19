25117f1a28b80af5f5cbcccdb75c932bf2b83daa
gustavobgt <gustavo_bgt97@hotmail.com>
2023-04-19 07:27:53 -0300

    feat: update user token schema

--- src/shared/typeorm/migrations/1681667839266-CreateUserTokens.ts
+++ src/shared/typeorm/migrations/1681667839266-CreateUserTokens.ts
@@ -23,6 +23,16 @@ export class CreateUserTokens1681667839266 implements MigrationInterface {
             name: 'user_id',
             type: 'uuid',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
         foreignKeys: [
           {
