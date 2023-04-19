d067e922a7f2e7e2438c2defdbab3a0e15261554
gustavobgt <gustavo_bgt97@hotmail.com>
2023-04-19 08:02:36 -0300

    feat: mail template with handlebars

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+    console.log(user);
+
+    if (!user) {
+      console.log('ENTROU AQUI');
+      throw new AppError('User does not exists.');
+    }
+
+    const userToken = await userTokensRepository.generate(user.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${userToken?.token}`,
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
